fb70bc60ff9eb299edb6aad9be0ece6fceff5715
Chris Turiano <cturiano@gmail.com>
2018-07-26 09:14:18 -0700

    First Commit

---
+++ Pulse8Core/Models/SqlDBManager.cs
@@ -0,0 +1,205 @@
+﻿using System;
+using System.Data;
+using System.Data.Common;
+using System.Data.SqlClient;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using Pulse8Core.Properties;
+
+namespace Pulse8Core.Models
+{
+    public class SqlDBManager
+    {
+        #region Static Fields and Constants
+
+        private static string _dbName;
+        private static string _sqlFilePath;
+
+        #endregion
+
+        #region Constructors
+
+        public SqlDBManager(string dbName, string sqlFilePath)
+        {
+            if (string.IsNullOrEmpty(sqlFilePath))
+            {
+                throw new ArgumentNullException(nameof(sqlFilePath), Resources.NoSqlFilePathMessage);
+            }
+
+            if (string.IsNullOrEmpty(dbName))
+            {
+                throw new ArgumentNullException(nameof(dbName), Resources.NoDBNameMessage);
+            }
+
+            if (!File.Exists(sqlFilePath))
+            {
+                throw new ArgumentException(nameof(sqlFilePath), string.Format(Resources.SqlFileDoesNotExistMessage, sqlFilePath));
+            }
+
+            _dbName = dbName;
+            _sqlFilePath = sqlFilePath;
+
+            if (!CreateDBAsync().Result)
+            {
+                throw new Exception(Resources.CouldNotCreateDBMessage);
+            }
+        }
+
+        #endregion
+
+        #region Public Methods
+
+        public async Task<bool> CreateDBAsync()
+        {
+            if (string.IsNullOrEmpty(_dbName) || string.IsNullOrEmpty(_sqlFilePath))
+            {
+                return false;
+            }
+
+            return await CreateDBAsync(_dbName, _sqlFilePath);
+        }
+
+        public static async Task<bool> CreateDBAsync(string dbName, string sqlFilePath)
+        {
+            var retVal = false;
+            if (!await DBExistsAsync(dbName))
+            {
+                using (var connection = new SqlConnection(Resources.SqlConnectionString))
+                {
+                    try
+                    {
+                        var sql = CleanScript(sqlFilePath);
+                        if (!string.IsNullOrEmpty(sql))
+                        {
+                            var sqls = sql.Split(';');
+                            var cmd = new SqlCommand(sqls[0], connection);
+                            
+                            await connection.OpenAsync().ConfigureAwait(false);
+                            await cmd.ExecuteNonQueryAsync().ConfigureAwait(false);
+                            
+                            foreach (var s in sqls.Skip(1).Where(s => !string.IsNullOrEmpty(s)))
+                            {
+                                cmd.CommandText = s;
+                                await cmd.ExecuteNonQueryAsync().ConfigureAwait(false);
+                            }
+
+                            retVal = true;
+                        }
+                    }
+                    catch (DbException de)
+                    {
+                        // log the exception
+                    }
+                    catch (ArgumentNullException ane)
+                    {
+                        // log the exception
+                    }
+                }
+            }
+            else
+            {
+                retVal = true;
+            }
+
+            return retVal;
+        }
+
+        public static async Task<bool> DBExistsAsync(string dbName)
+        {
+            using (var connection = new SqlConnection(Resources.SqlConnectionString))
+            {
+                using (var command = new SqlCommand($"SELECT db_id('{dbName}')", connection))
+                {
+                    await connection.OpenAsync().ConfigureAwait(false);
+                    return await command.ExecuteScalarAsync().ConfigureAwait(false) != DBNull.Value;
+                }
+            }
+        }
+
+        public static async Task DropDBAsync(string dbName)
+        {
+            if (await DBExistsAsync(dbName))
+            {
+                using (var connection = new SqlConnection(Resources.SqlConnectionString))
+                {
+                    SqlConnection.ClearAllPools();
+                    await connection.OpenAsync().ConfigureAwait(false);
+                    using (var cmd = new SqlCommand($"ALTER DATABASE {dbName} SET SINGLE_USER WITH ROLLBACK IMMEDIATE;", connection) {CommandType = CommandType.Text})
+                    {
+                        await cmd.ExecuteNonQueryAsync().ConfigureAwait(false);
+                    }
+
+                    using (var cmd = new SqlCommand($"IF EXISTS (SELECT name FROM sys.databases WHERE name = N'{dbName}') DROP DATABASE {dbName};", connection))
+                    {                                                  
+                        await cmd.ExecuteNonQueryAsync().ConfigureAwait(false);
+                    }
+                }
+            }
+        }
+
+        public static async Task<int> ExecuteNonQueryAsync(string query)
+        {
+            using (var connection = new SqlConnection(Resources.SqlConnectionString2))
+            {
+                using (var command = new SqlCommand(query, connection))
+                {
+                    await connection.OpenAsync().ConfigureAwait(false);
+                    return await command.ExecuteNonQueryAsync().ConfigureAwait(false);
+                }
+            }
+        }
+
+        public static async Task<DataTable> ExecuteReaderAsync(string query)
+        {
+            var dt = new DataTable();
+            using (var connection = new SqlConnection(Resources.SqlConnectionString2))
+            {
+                using (var command = new SqlCommand(query, connection))
+                {                    
+                    await connection.OpenAsync().ConfigureAwait(false);
+                    dt.Load(await command.ExecuteReaderAsync().ConfigureAwait(false));
+                }
+            }
+
+            return dt;
+        } 
+        
+        public static async Task<object> ExecuteScalarAsync(string query)
+        {
+            using (var connection = new SqlConnection(Resources.SqlConnectionString2))
+            {
+                using (var command = new SqlCommand(query, connection))
+                {
+                    await connection.OpenAsync().ConfigureAwait(false);
+                    return await command.ExecuteScalarAsync().ConfigureAwait(false);
+                }
+            }
+        } 
+
+        #endregion
+
+        #region Private Methods
+
+        private static string CleanScript(string sqlFilePath)
+        {
+            try
+            {
+                var sql = File.ReadAllText(sqlFilePath);
+                if (!string.IsNullOrEmpty(sql))
+                {
+                    return Regex.Replace(sql, "\r\nGO\r\n", ";").Replace("\r\n", string.Empty).Replace("\t", string.Empty);
+                }
+            }
+            catch (Exception e)
+            {
+                // log the exception
+            }
+
+            return string.Empty;
+        }
+
+        #endregion
+    }
+}
---
+++ Pulse8CoreTests/ModelsTests/SqlDBCreatorTests.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using Pulse8Core.Models;
+
+// ReSharper disable ObjectCreationAsStatement
+
+namespace Pulse8CoreTests.ModelsTests
+{
+    [TestFixture]
+    public class SqlDBManagerTests
+    {
+        #region Setup/Teardown
+
+        [TearDown]
+        public async Task TearDown()
+        {
+           await SqlDBManager.DropDBAsync(DBName);
+        }
+
+        #endregion
+
+        private async Task Setup()
+        {
+            await SqlDBManager.CreateDBAsync(DBName, _sqlFile);
+        }
+
+        private const string DBName = "Pulse8TestDB";
+        private readonly string _sqlFile = Path.Combine(AppDomain.CurrentDomain.SetupInformation.ApplicationBase, "..", "..", "..", "Pulse8Core", "Pulse8TestDBSetup.sql");
+
+        [Test]
+        public async Task ConstructorAndPropertiesTests()
+        {
+            try
+            {
+                new SqlDBManager(null, _sqlFile);
+                Assert.Fail("Should have thrown");
+            }
+            catch (ArgumentNullException)
+            {
+            }
+
+            try
+            {
+                new SqlDBManager(DBName, null);
+                Assert.Fail("Should have thrown");
+            }
+            catch (ArgumentNullException)
+            {
+            }
+
+            try
+            {
+                new SqlDBManager(DBName, "blah");
+                Assert.Fail("Should have thrown");
+            }
+            catch (ArgumentException)
+            {
+            }
+
+            var sdc = new SqlDBManager(DBName, _sqlFile);
+            Assert.IsNotNull(sdc);
+            Assert.IsTrue(await SqlDBManager.DBExistsAsync(DBName));
+        }
+
+        [Test]
+        public async Task CreateDropDBTest()
+        {
+            Assert.IsFalse(await SqlDBManager.DBExistsAsync(DBName));
+            await SqlDBManager.CreateDBAsync(DBName, _sqlFile);
+            Assert.IsTrue(await SqlDBManager.DBExistsAsync(DBName));
+            await SqlDBManager.DropDBAsync(DBName);  
+            Assert.IsFalse(await SqlDBManager.DBExistsAsync(DBName));
+        }
+
+        [Test]
+        public async Task DBExistsTest()
+        {
+            Assert.IsFalse(await SqlDBManager.DBExistsAsync(DBName));
+            await Setup();
+            Assert.IsTrue(await SqlDBManager.DBExistsAsync(DBName));
+        }
+        
+        [Test]
+        public async Task ExecuteNonQueryTest()
+        {                                                                                                                                                                       
+            await Setup();
+            Assert.AreEqual(5, await SqlDBManager.ExecuteScalarAsync("SELECT COUNT(*) FROM Diagnosis;"));
+            Assert.AreEqual(1, await SqlDBManager.ExecuteNonQueryAsync("INSERT INTO Diagnosis ( DiagnosisID , DiagnosisDescription )VALUES  ( 6 , 'Test Diagnosis 6' );"));
+            Assert.AreEqual(6, await SqlDBManager.ExecuteScalarAsync("SELECT COUNT(*) FROM Diagnosis;"));
+        }
+        
+        [Test]
+        public async Task ExecuteScalarTest()
+        {                                                                                                                                                                       
+            await Setup();
+            Assert.AreEqual(3, await SqlDBManager.ExecuteScalarAsync("SELECT COUNT(*) FROM DiagnosisCategory;"));
+        }
+        
+        [Test]
+        public async Task ExecuteReaderTest()
+        {                                                                                                                                                                       
+            await Setup();
+            var table = await SqlDBManager.ExecuteReaderAsync("SELECT * FROM DiagnosisCategory;");
+            Assert.IsNotNull(table);
+            Assert.AreEqual(3, table.Rows.Count);
+        }
+
+        [TestCase("SELECT * FROM Member WHERE FirstName='John';", "1", "John", "Smith")]
+        [TestCase("SELECT * FROM DiagnosisCategoryMap WHERE DiagnosisID='1';", "1", "1")]
+        [TestCase("SELECT * FROM DiagnosisCategory WHERE CategoryScore='10';", "1", "Category A" ,"10")]
+        [TestCase("SELECT * FROM Diagnosis WHERE DiagnosisID='1';", "1", "Test Diagnosis 1")]
+        [TestCase("SELECT * FROM MemberDiagnosis WHERE DiagnosisID='2';", "1", "2")]
+        [TestCase("SELECT * FROM Member WHERE MemberID = (SELECT MemberID FROM MemberDiagnosis WHERE DiagnosisID = 3);", "3", "Will", "Smyth")]
+        [TestCase("SELECT * FROM Member WHERE NOT EXISTS (SELECT MemberID FROM MemberDiagnosis WHERE Member.MemberID = MemberDiagnosis.MemberID);", "2", "Jack", "Smith")]
+        public async Task MiscQueryTest(string query, params string[] expected)
+        {
+            await Setup();
+            var result = await SqlDBManager.ExecuteReaderAsync(query);
+
+            for (var i = 0; i < expected.Length; i++)
+            {
+                Assert.AreEqual(expected[i], result.Rows[0][i].ToString());
+            }
+        }
+    }
+}
